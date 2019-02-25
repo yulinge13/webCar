ff043b5b0b1bb25d54d7592ca51a5b70770b10f1
linge.l.yu@cn.pwc.com <linge.l.yu@cn.pwc.com>
2019-02-25 20:42:30 +0800

    'up'

--- db/distributor.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `distributor` VALUES (1, '123', '123', '123', '213', '213', '213', '213');
-INSERT INTO `distributor` VALUES (2, '123', '123', '123', '213', '213', '213', '213');
-INSERT INTO `distributor` VALUES (3, '123', '阿斯顿撒旦', '123', '213', '213', '213', '撒旦撒旦 ');
